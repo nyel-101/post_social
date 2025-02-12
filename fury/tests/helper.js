dbafc7502f40619f970c0469742e7345aad4ec0f
nyel-101 <dandevjr@gmail.com>
2025-02-12 17:30:28 +0000

    refactor(worker): decouple auth tokens

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_272ea6d3422b44dabfd657586cc760ff = () => {
-  return '2026-06-05T10:20:41Z';
+const node_8ab4383a207840e5a6c7cc59e0383279 = () => {
+  return '2025-02-12T17:30:28Z';
 };
