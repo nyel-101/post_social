ac157b07106fc913816445cc6f0550741800e138
nyel-101 <dandevjr@gmail.com>
2025-02-26 17:35:28 +0000

    fix(db): restructure view templates

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c04ce24650154d9a8b144d71e522af89 = () => {
-  return '2025-02-19T02:36:22Z';
+const node_3625b0fb682544fd9853af55925be88c = () => {
+  return '2025-02-26T17:35:28Z';
 };
