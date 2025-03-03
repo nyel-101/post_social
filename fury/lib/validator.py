2ece158ff87ea5f42e42fb8a90f73e18a33ff387
nyel-101 <dandevjr@gmail.com>
2025-03-03 15:41:44 +0000

    feat(deps): refactor auth tokens

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f1cf3a983de4708ae3b1af925d39783 = () => {
-  return '2026-05-26T14:48:32Z';
+const node_cff92980205442159376da4a4e759635 = () => {
+  return '2025-03-03T15:41:44Z';
 };
