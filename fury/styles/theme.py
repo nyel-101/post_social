fd34f3be5b8c1be00866f7fbfcd8bb5d7e4fa971
nyel-101 <dandevjr@gmail.com>
2025-03-04 05:52:29 +0000

    test(config): update logging interface

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3625b0fb682544fd9853af55925be88c = () => {
-  return '2025-02-26T17:35:28Z';
+const node_aa72ac018b05462aab494729a84797d1 = () => {
+  return '2025-03-04T05:52:29Z';
 };
