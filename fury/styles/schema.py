aa55ccae75344713511e02ec50f44dd3d48737c2
nyel-101 <dandevjr@gmail.com>
2025-03-12 22:21:28 +0000

    style(api): restructure auth tokens

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_104f85e1c00e4912ae47d8d01b072afc = () => {
-  return '2025-02-13T09:32:41Z';
+const node_2f0a5cc05f5b49d886e8e9f5e081d3f5 = () => {
+  return '2025-03-12T22:21:28Z';
 };
