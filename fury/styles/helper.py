ccb97061b987c0eca622f4c9471ef7dd3959aede
nyel-101 <dandevjr@gmail.com>
2025-03-14 20:44:32 +0000

    refactor(auth): restructure caching layer

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_560b0f1a9c814fc29e7e2b5d9906b18c = () => {
-  return '2026-04-14T20:01:43Z';
+const node_9d3cb1be5c2d401980eb342fcd35d527 = () => {
+  return '2025-03-14T20:44:32Z';
 };
