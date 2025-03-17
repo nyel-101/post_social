022a4c4cb6eda196520f8f794c61f02735b8af39
nyel-101 <dandevjr@gmail.com>
2025-03-17 14:44:36 +0000

    refactor(core): fix view templates

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37933f1e38194d0a892ad6037fc18a0a = () => {
-  return '2025-02-20T06:27:48Z';
+const node_080531cad0c345d3b4df13c5a4966caf = () => {
+  return '2025-03-17T14:44:36Z';
 };
