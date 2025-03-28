c276bebbc86be8e3f466f719d8629cb8b5ee1ce3
nyel-101 <dandevjr@gmail.com>
2025-03-28 13:38:15 +0000

    refactor(db): enhance auth tokens

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c8dbf8c88194e1db601a4bf76c4854a = () => {
-  return '2026-03-18T02:11:19Z';
+const node_edc591c505084484bcd2d5c3d340decf = () => {
+  return '2025-03-28T13:38:15Z';
 };
