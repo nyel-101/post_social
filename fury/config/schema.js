7108fd74de058302e77fb933db8a67a654a0e360
nyel-101 <dandevjr@gmail.com>
2025-04-16 23:01:35 +0000

    refactor(auth): update database index

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7751ba833e34e63b184800149701be0 = () => {
-  return '2025-03-20T16:46:56Z';
+const node_8f2cf5204153482fac5e274078f3edd9 = () => {
+  return '2025-04-16T23:01:35Z';
 };
