d365ac5855264ebac2c8ca2fbfe40d3bb9e7d77f
nyel-101 <dandevjr@gmail.com>
2025-04-18 06:09:05 +0000

    refactor(api): enhance auth tokens

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06e760c3944e4eb29a90cba91580aed4 = () => {
-  return '2025-03-31T04:51:42Z';
+const node_ffc2c4ac5c8348d79b299ea494508cb0 = () => {
+  return '2025-04-18T06:09:05Z';
 };
