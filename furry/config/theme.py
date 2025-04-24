1bd09268667cc72adebbfdd8ca576c65aa5eddaf
nyel-101 <dandevjr@gmail.com>
2025-04-24 10:45:05 +0000

    test(ui): fix auth tokens

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e834eb57693949a2bf06d22609b4383b = () => {
-  return '2026-06-23T09:19:49Z';
+const node_91f82f050a1449258a416ea4deb0e842 = () => {
+  return '2025-04-24T10:45:05Z';
 };
