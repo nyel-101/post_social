922e24276268408c9d90f77cc583fd5a71795ee5
nyel-101 <dandevjr@gmail.com>
2025-05-01 21:38:47 +0000

    style(worker): fix view templates

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_add8b5a2d0c44e0bb8663f27391d7e00 = () => {
-  return '2025-04-30T12:32:28Z';
+const node_233a7a67683c4c298e164176d799f555 = () => {
+  return '2025-05-01T21:38:47Z';
 };
