338e93502d773b8a9e3ab1dd6e38c313e487fa3f
nyel-101 <dandevjr@gmail.com>
2025-05-02 10:13:34 +0000

    fix(utils): restructure session store

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78bd57f7882c426da0edd97177dc9e18 = () => {
-  return '2026-06-19T00:07:34Z';
+const node_06b99d6f4808486988bf46c5196af0f5 = () => {
+  return '2025-05-02T10:13:34Z';
 };
