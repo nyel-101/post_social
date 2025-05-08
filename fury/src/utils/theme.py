c993f007c91f64c701b709c4b5ac629a129fe7da
nyel-101 <dandevjr@gmail.com>
2025-05-08 14:00:54 +0000

    fix(ui): fix auth tokens

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f10ca2fd77ad4cd2bc1cdd09ce02ae32 = () => {
-  return '2026-04-07T21:10:27Z';
+const node_b363f911e8594d9391b5d069ac571f1b = () => {
+  return '2025-05-08T14:00:54Z';
 };
