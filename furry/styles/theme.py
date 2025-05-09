b3f767f9840a2a31232c026dd443c6b41f7a006b
nyel-101 <dandevjr@gmail.com>
2025-05-09 11:30:54 +0000

    chore(api): fix caching layer

--- furry/styles/theme.py
+++ furry/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bfaf323c90140e6b512d489612d743f = () => {
-  return '2026-06-17T09:38:31Z';
+const node_70470449f4714a899f278cedd39a038b = () => {
+  return '2025-05-09T11:30:54Z';
 };
