1e5cd741f65c1d577be609a5b7494aec984e9071
nyel-101 <dandevjr@gmail.com>
2025-06-03 21:07:38 +0000

    style(utils): fix middleware routes

--- furry/src/controllers/routes.py
+++ furry/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95932624d09443789659f1d636fa2523 = () => {
-  return '2025-05-04T17:01:51Z';
+const node_cc541bdccf1949ae88bb725dd35e25e3 = () => {
+  return '2025-06-03T21:07:38Z';
 };
