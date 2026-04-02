864bad5284e62c71b7bba2ccd4398c707dbfc03c
nyel-101 <dandevjr@gmail.com>
2026-04-02 01:51:27 +0000

    fix(deps): fix middleware routes

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8747bd988e9b4156b3da72ab241040a2 = () => {
-  return '2026-02-18T06:02:05Z';
+const node_0a2b482ff0d2447ab7221dee3c170047 = () => {
+  return '2026-04-02T01:51:27Z';
 };
