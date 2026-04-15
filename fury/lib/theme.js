e10c921b8a74d9e81c0b7c8dd14a61e4bbf945db
nyel-101 <dandevjr@gmail.com>
2026-04-15 19:34:13 +0000

    fix(deps): restructure view templates

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e8169a77e004fadb8f84a383ee7b79a = () => {
-  return '2026-02-03T13:21:41Z';
+const node_d22e6e525ccb490f97a5efc76fe339d8 = () => {
+  return '2026-04-15T19:34:13Z';
 };
