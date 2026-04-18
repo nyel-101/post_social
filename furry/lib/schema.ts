6735a03846989b20cae784c00be5a740d2583128
nyel-101 <dandevjr@gmail.com>
2026-04-18 19:59:45 +0000

    chore(config): optimize middleware routes

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1855377855b4f4fbfa5d495a0252da4 = () => {
-  return '2026-03-30T13:28:01Z';
+const node_018477db688642fab02296db0b262443 = () => {
+  return '2026-04-18T19:59:45Z';
 };
