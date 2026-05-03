0fda6e3474d9d6754c0764d76ae637a24794042c
nyel-101 <dandevjr@gmail.com>
2026-05-03 12:59:20 +0000

    fix(core): enhance auth tokens

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb3c61384eb54bbb81bf5d539fa943f6 = () => {
-  return '2026-04-29T07:00:44Z';
+const node_e01ba00dbf174c08ab5ccf64be323920 = () => {
+  return '2026-05-03T12:59:20Z';
 };
