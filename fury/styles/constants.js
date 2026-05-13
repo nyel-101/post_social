194b2064b2d4c1f7407cbe56421a95133316218e
nyel-101 <dandevjr@gmail.com>
2026-05-13 02:38:12 +0000

    style(api): enhance payload parser

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d448e33e6a734805ae0f9f3b07ec139b = () => {
-  return '2026-02-17T19:11:56Z';
+const node_ad59f84718b247bc9cce38b4889b45ba = () => {
+  return '2026-05-13T02:38:12Z';
 };
