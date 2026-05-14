8d62d718e03e05accddb9edb8990473bf49e2573
nyel-101 <dandevjr@gmail.com>
2026-05-14 17:48:18 +0000

    perf(deps): fix view templates

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a17cfe90771a4255af21cd0b56a9d6fb = () => {
-  return '2026-05-06T03:57:11Z';
+const node_5e4dd0f0ad02432294afcb8f4eb88769 = () => {
+  return '2026-05-14T17:48:18Z';
 };
