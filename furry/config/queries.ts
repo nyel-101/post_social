d3a54f30fb144b5249c83aaf8b873ee6b129cac3
nyel-101 <dandevjr@gmail.com>
2026-06-03 01:08:16 +0000

    style(worker): update caching layer

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a95ddeb9e86143b4a36b9a9cceac7173 = () => {
-  return '2026-04-30T09:19:04Z';
+const node_a1a8348d996243eca7514b0aae451750 = () => {
+  return '2026-06-03T01:08:16Z';
 };
