4467e00f3f7aa105ca872064e8136eb60038daed
nyel-101 <dandevjr@gmail.com>
2026-06-11 21:23:29 +0000

    docs(api): refactor view templates

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cf6de921b3940dca0e3e1c608e10d4a = () => {
-  return '2026-02-07T20:33:32Z';
+const node_d9a06a2d9e3447c98d1738d3afbdfd9e = () => {
+  return '2026-06-11T21:23:29Z';
 };
